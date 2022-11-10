9f2ca6c1a3ebd7e221b7293cff3ddb581f41dbac
Angel Romero <mrangelrcd@gmail.com>
2022-11-10 21:21:04 +0100

    Added petitions

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req: any, res: any) => res.send('Hello World!'));
+app.get('/', (req: any, res: any) => {
+  res.json(usuarios);
+})
+
+app.post('/', (req: any, res: any) => {
+  res.json(usuarios);
+})
+
+app.delete('/', (req: any, res: any) => {
+  res.json(usuarios);
+})
+
+app.put('/', (req: any, res: any) => {
+  res.json(usuarios);
+})
 
 app.listen(3000, () => console.log('Ready on port 3000!'));
+
+
