efeb0932632dcb7aae07d0086d1b5b4a70b0040b
Angel Romero <mrangelrcd@gmail.com>
2022-11-10 21:30:50 +0100

    Read added

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,27 @@
 const express = require('express');
 const app = express();
 
+const movies = [
+  {
+    title: "peli1",
+    likes: "0",
+  }
+]
+
 app.get('/', (req: any, res: any) => {
-  res.json(usuarios);
+  res.json(movies);
 })
 
 app.post('/', (req: any, res: any) => {
-  res.json(usuarios);
+  res.json(movies);
 })
 
 app.delete('/', (req: any, res: any) => {
-  res.json(usuarios);
+  res.json(movies);
 })
 
 app.put('/', (req: any, res: any) => {
-  res.json(usuarios);
+  res.json(movies);
 })
 
 app.listen(3000, () => console.log('Ready on port 3000!'));
