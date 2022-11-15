7d36c2cff495b536d1fc8ebd6d50bdcb3203bf25
Antonio Bogallo <antonio.bogallo@gmail.com>
2022-11-15 13:21:30 +0100

    create and read endpoints created

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-const express = require('express');
+import express from 'express'
+import { STATUS_CODES } from 'http';
 const app = express();
+app.use(express.json())
 
 const movies = [
   {
@@ -12,8 +14,29 @@ app.get('/', (req: any, res: any) => {
   res.json(movies);
 })
 
-app.post('/', (req: any, res: any) => {
-  res.json(movies);
+app.post('/addMovie', (req: any, res: any) => {
+  if (req.body.constructor === Object && Object.keys(req.body).length === 0){
+    res.status(404).send("The json is empty");
+  }
+
+  if(!req.body.title){
+    res.status(404).send("The title not is in the json")
+  }
+
+  if(!req.body.likes){
+    const movie = {
+      title: `${req.body.title}`,
+      likes: "0"
+    }
+    movies.push(movie);
+  }else{
+    const movie = {
+      title: `${req.body.title}`,
+      likes: `${req.body.likes}`
+    }
+    movies.push(movie);
+  }
+  res.send(movies);
 })
 
 app.delete('/', (req: any, res: any) => {
